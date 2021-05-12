658b30238c3269382a3671f65875146cd0c2d8ad
Muhajir M.R <mohamemr@amazon.com>
2021-05-12 07:37:56 -0700

    File program

---
+++ src/org/file/Ques1.java
@@ -0,0 +1,21 @@
+package org.file;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+
+public class Ques1 {
+public static void main(String[] args) throws IOException {
+	File f=new File("C:\\java\\java.txt");
+	boolean a = f.mkdir();
+	System.out.println(a);
+	
+	boolean b = f.createNewFile();
+	System.out.println(b);
+	
+	
+	
+	
+}
+}
